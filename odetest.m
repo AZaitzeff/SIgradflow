d7daab3c2b1eba0903b16691251ed5c235e6c68e
Alexander Zaitzeff <azaitzeff@gmail.com>
2019-09-20 13:28:48 -0400

    the tests

---
+++ odetest.m
@@ -0,0 +1,36 @@
+%addpath('../findgamma/')
+%[gamma,m]=getgamma(3,0);
+
+T=2;
+trueval=2*atan(exp(-T));
+Ns=[16,32,64,128,256,512,1024,2048,4096];
+maxiter=1000;
+n=numel(Ns);
+error1=zeros(2,n);
+for iter=1:n
+    N=Ns(iter);
+    k=T/N;
+    t=0:k:T;
+    u=zeros(N,m+1);
+    
+    for i=1:N
+        if i==1
+          u(1,1)=pi/2;  
+        else
+          u(i,1)=u(i-1,m+1);
+        end
+        %u(i+1)=u(i)/(1+k);
+        for j=2:m+1
+            ubar=sum(gamma(j-1,1:j-1).*u(i,1:j-1));
+            S=sum(gamma(j-1,1:j-1));
+            curu=1/(S+2*k)*(-k*sum((sin(u(i,1:j-1))-2*u(i,1:j-1)).*theta(j-1,1:j-1))+ubar);
+            u(i,j)=curu;
+        end
+    end
+
+    error1(1,iter)=abs(u(N,m+1)-trueval);
+end
+
+for i=2:n
+    error1(2,i)=(log(error1(1,i-1))-log(error1(1,i)))/log(2);
+end
---
+++ sieac.m
@@ -0,0 +1,39 @@
+
+nt=2^22;
+N=4096;
+%nt=2^18;
+%N=2048;
+T=20;
+eps=1;
+[U,X,Y,h]=initializebigcircle(N,eps);
+Uinit=U;
+dt=T/nt;
+[l,k]=meshgrid(0:(N-1));
+invmatrix0=1-dt/(h^2)*(-5+8/3*cos(2*pi*l/N)+8/3*cos(2*pi*k/N)-1/6*cos(4*pi*l/N)-1/6*cos(4*pi*k/N));
+invmatrix=3/2-dt/(h^2)*(-5+8/3*cos(2*pi*l/N)+8/3*cos(2*pi*k/N)-1/6*cos(4*pi*l/N)-1/6*cos(4*pi*k/N));
+%invmatrix=1-dt/(2*h^2)*(-4976/1152+3/2*(cos(2*pi*l/N)+cos(2*pi*k/N))...
+%    +3/40*(cos(4*pi*l/N)+cos(4*pi*k/N))+1/45*(cos(6*pi*l/N)+cos(6*pi*k/N))...
+%    +2*(cos(2*pi*l/N)*cos(2*pi*k/N))+1/8*(cos(4*pi*l/N)*cos(4*pi*k/N))...
+%    -1/2*(cos(4*pi*l/N)*cos(2*pi*k/N)+cos(2*pi*l/N)*cos(4*pi*k/N)));
+tic;
+Upre=U;
+for t=1:nt
+    if t==1
+        F=U-dt*1/eps^2*(2*U-6*U.^2+4*U.^3);
+        Fbar=fft2(F);
+        Ubar=Fbar./(invmatrix0);
+        U=real(ifft2(Ubar));
+    else
+        
+        F=2*U-1/2*Upre-2*dt*1/eps^2*(2*U-6*U.^2+4*U.^3)+dt*1/eps^2*(2*Upre-6*Upre.^2+4*Upre.^3);
+        Upre=U;
+        Fbar=fft2(F);
+        Ubar=Fbar./(invmatrix);
+        U=real(ifft2(Ubar));
+    end
+    %F=U-dt*1/eps^2*(2*U-6*U.^2+4*U.^3);
+    
+    
+end
+toc;
+save(['results/si2ac' num2str(nt) 'N' num2str(N)],'T','dt','N','U','eps')
